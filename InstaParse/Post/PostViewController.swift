c11588f1c6e6d37b0ad1817b048850c85e8a59c4
Halil İLHAN <47319650+hybrayhem@users.noreply.github.com>
2024-02-03 01:38:12 +0300

    add image picker

--- InstaParse/Post/PostViewController.swift
+++ InstaParse/Post/PostViewController.swift
@@ -6,10 +6,7 @@
 //
 
 import UIKit
-
-// TODO: Import Photos UI
-
-// TODO: Import Parse Swift
+import PhotosUI
 
 class PostViewController: UIViewController {
 
@@ -25,18 +22,21 @@ class PostViewController: UIViewController {
     }
 
     @IBAction func onPickedImageTapped(_ sender: UIBarButtonItem) {
-        // TODO: Pt 1 - Present Image picker
+        // Create picker configuration
+        var config = PHPickerConfiguration()
+        config.filter = .images
+        config.selectionLimit = 1
+        config.preferredAssetRepresentationMode = .current // no transcoding, get original file
 
+        // Instantiate and present the picker
+        let picker = PHPickerViewController(configuration: config)
+        picker.delegate = self
+        present(picker, animated: true)
     }
 
     @IBAction func onShareTapped(_ sender: Any) {
-
         // Dismiss Keyboard
         view.endEditing(true)
-
-        // TODO: Pt 1 - Create and save Post
-
-
     }
 
     @IBAction func onViewTapped(_ sender: Any) {
@@ -50,7 +50,36 @@ class PostViewController: UIViewController {
         alertController.addAction(action)
         present(alertController, animated: true)
     }
+    
+    private func showAlert(for error: Error) {
+        showAlert(description: error.localizedDescription)
+    }
 }
 
-// TODO: Pt 1 - Add PHPickerViewController delegate and handle picked image.
-
+extension PostViewController: PHPickerViewControllerDelegate {
+    func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+        picker.dismiss(animated: true)
+        
+        guard let provider = results.first?.itemProvider,
+              provider.canLoadObject(ofClass: UIImage.self) else { return }
+        
+        // Load item
+        provider.loadObject(ofClass: UIImage.self) { [weak self] object, error in
+            
+            guard let image = object as? UIImage else {
+                self?.showAlert() // Unable to cast to UIImage
+                return
+            }
+            
+            if let error = error {
+                self?.showAlert(for: error)
+            } else {
+                // Set image
+                DispatchQueue.main.async {
+                    self?.pickedImage = image
+                    self?.previewImageView.image = image
+                }
+            }
+        }
+    }
+}
